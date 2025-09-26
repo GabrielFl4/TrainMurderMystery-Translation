f41cf3eb2cbe87980e582c6fde29fdd4ae974082
Laptop Hollowed <hollowedwanderer@gmail.com>
2025-09-25 21:57:49 -0400

    slightly working firecracker I guess

--- src/main/java/dev/doctor4t/trainmurdermystery/entity/FirecrackerEntity.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/entity/FirecrackerEntity.java
@@ -1,14 +1,29 @@
 package dev.doctor4t.trainmurdermystery.entity;
 
+import dev.doctor4t.trainmurdermystery.index.TMMSounds;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.world.World;
 
 public class FirecrackerEntity extends Entity {
+    private static final int TIMER = 30;
+
     public FirecrackerEntity(EntityType<?> type, World world) {
         super(type, world);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        this.getWorld().addParticle(ParticleTypes.SMOKE, this.getX(), this.getY(), this.getZ(), 0, 0.04, 0);
+        if (this.age >= TIMER) {
+            this.getWorld().playSound(null, this.getBlockPos(), TMMSounds.ITEM_REVOLVER_SHOOT, SoundCategory.PLAYERS, 5f, 1f + this.getRandom().nextFloat() * .1f - .05f);
+            this.discard();
+        }
     }
 
     @Override
